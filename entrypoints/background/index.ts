a2e7f4b3f66ba60ffe1a649d82a019977de80b1b
Colin <cwrmcolin@gmail.com>
2025-05-15 20:07:28 -0700

    dev(setup): on-install welcome page

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -4,5 +4,16 @@ export default defineBackground({
   // Executed when background is loaded, CANNOT BE ASYNC
   main() {
     console.log('Hello background!', { id: browser.runtime.id });
+
+    // Open the welcome page when the extension is first installed
+    browser.runtime.onInstalled.addListener((details) => {
+      console.log('onInstalled', details);
+
+      if (details.reason === 'install') {
+        browser.tabs.create({
+          url: 'welcome-page.html',
+        });
+      }
+    });
   },
 });
